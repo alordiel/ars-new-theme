37de1bef6e8cfc16ae25c7ea67c2ca1480f65836
Aleksandar Vasilev <alexander.vasilev@protonmail.com>
2022-03-13 14:22:05 +0200

    moved in the shortcodes for donations

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 /**
  * Child-Theme functions and definitions
  */
+include_once 'functions/shortcodes.php';
+
 
 function impacto_patronus_child_scripts() {
     wp_enqueue_style( 'impacto-patronus-parent-style', get_template_directory_uri(). '/style.css' );
---
+++ functions/shortcodes.php
@@ -0,0 +1,16 @@
+<?php
+add_shortcode( 'donation', 'shortcode_for_donations_for_the_farm' );
+function shortcode_for_donations_for_the_farm() {
+	ob_start();
+	include_once( '../page-parts/donations-embed-farm.php' );
+
+	return ob_get_clean();
+}
+
+add_shortcode( 'donation-horses', 'shortcode_for_donation_for_the_ranch' );
+function shortcode_for_donation_for_the_ranch() {
+	ob_start();
+	include_once( '../page-parts/donations-embed-ranch.php' );
+
+	return ob_get_clean();
+}
